0d44da67b32d0e5ca2efcbe7d5e7ec8e6957efe7
matheus.silva <tecmatheusg@hotmail.com>
2023-05-05 14:01:56 -0300

    first commit

---
+++ Estacionamento.Testes/VeiculoTestes.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace Estacionamento.Testes
+{
+    public class VeiculoTestes
+    {
+        [Fact]
+        public void Test()
+        {
+
+        }
+
+        public void Test1()
+        {
+
+        }
+    }
+}
