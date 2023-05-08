dcb7839de49a6e149cfd9f0fc8833f34902ecb86
matheus.silva <tecmatheusg@hotmail.com>
2023-05-08 14:20:15 -0300

    Padrão AAA

--- Estacionamento.Testes/VeiculoTestes.cs
+++ Estacionamento.Testes/VeiculoTestes.cs
@@ -1,3 +1,4 @@
+using Alura.Estacionamento.Alura.Estacionamento.Modelos;
 using Alura.Estacionamento.Modelos;
 using System;
 using Xunit;
@@ -9,17 +10,34 @@ namespace Estacionamento.Testes
         [Fact]
         public void TestaVeiculoAcelerar()
         {
+            //arrange
             var veiculo = new Veiculo();
+            //act
             veiculo.Acelerar(10);
+            //assert
             Assert.Equal(100, veiculo.VelocidadeAtual);
         }
 
         [Fact]
         public void TestaVeiculoFrear()
         {
+            //arrange
             var veiculo = new Veiculo();
+            //act
             veiculo.Frear(10);
+            //assert
             Assert.Equal(-150, veiculo.VelocidadeAtual);
+        }
+
+        [Fact]
+        public void TestaTipoVeiculo()
+        {
+            //arrange
+            var veiculo = new Veiculo();
+            //act
+            veiculo.Tipo = (TipoVeiculo) 1;
+            //assert
+            Assert.Equal("Automovel", veiculo.Tipo.ToString());
         }
     }
 }
